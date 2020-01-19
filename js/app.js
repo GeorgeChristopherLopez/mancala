563dfee7d5c7badad5dbaceb6f91431821cae60b
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2020-01-19 00:34:57 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Elements
 let board = document.querySelector('.board');
 let pits = Array.from(document.querySelectorAll('li'));
 let playerMankala = document.getElementById('player-mankala');
@@ -6,6 +7,11 @@ let playerPits = document.querySelector('.player-pits');
 let computerPits = document.querySelector('.computer-pits');
 let cTurn = document.querySelector('.cTurn');
 let pTurn = document.querySelector('.pTurn');
+let cScore = document.querySelector('.cScore');
+let pScore = document.querySelector('.pScore');
+
+
+// board values
 let arrayValue = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
 
 
@@ -22,27 +28,33 @@ for (pit of pcPits) {
 
 
 
-// starting value
+// VIEW UPDATE display
 let displayUpdate = () => {
     for (let x = 0; x < array.length; x++) {
         array[x].innerHTML = `<div class="container">${arrayValue[x]}</div>`;
     }
+    pTurn.classList.toggle('turn');
+    cTurn.classList.toggle('turn');
 
+    pScore.innerHTML = `Player Score: ${arrayValue[6]}`;
+    cScore.innerHTML = `Computer Score: ${arrayValue[13]}`;
 }
-let turnHandler = () => {
 
-}
 displayUpdate();
 
+
+
+// CONTROLLER FUNCTION
 board.addEventListener('click', e => {
-   /*******************/
-    /*if (pTurn.classList.contains('turn')) {
-    }
-    let checkTurn;
-    turn == pTurn ? checkTurn = `` : checkTurn = ``; 
-    */
+    // SIDE CHECKER
+    let currentSide;
+    pTurn.classList.contains('turn') ? 
+        currentSide = playerPits :
+        currentSide = computerPits;
+
+    // CLICK CHECKER
     let clicked = event.target;
-    if (array.includes(clicked) && clicked.classList.contains('mankala')==false) {
+    if (array.includes(clicked) && clicked.classList.contains('mankala') == false && clicked.parentNode == currentSide) {
         let i = array.indexOf(clicked);
         if (arrayValue[i] > 0) {
             for (let y = 1; arrayValue[i] > 0; y++) {
@@ -52,11 +64,10 @@ board.addEventListener('click', e => {
                 }
                 arrayValue[i + y]++; 
                 arrayValue[i]--;
-                console.log(arrayValue[i]);
             }
         }
-        pTurn.classList.toggle('turn');
-        cTurn.classList.toggle('turn');
+
+
         displayUpdate();
     };
 });
